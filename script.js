18ece62786434dea3637eca7ad8e8d00ecaf7bf0
maxiwer <fma1cr@gmail.com>
2021-01-26 15:50:39 +0600

    check lengths and password match

--- script.js
+++ script.js
@@ -29,8 +29,31 @@ function showSuccess(el) {
     formCtrl.className = 'form-control success';
 }
 
+// returns "`${field}` is required message"
 function requiredMessage(field) {
     return field.id[0].toUpperCase() + field.id.slice(1) + ' is required';
+}
+
+// returns "`${field}` must be at least `${min}` characters"
+function atLeastCharMsg(field, min) {
+    return `${field.id} must be at least ${min} characters`;
+}
+
+// returns "`${field}` must be at less than `${max}` characters"
+function lessThanCharMsg(field, max) {
+    return `${field.id} must be less than ${max} characters`;
+}
+
+// check input's length
+function checkLength(input, min, max) {
+    const fieldLength = input.value.length;
+    if (fieldLength < min) {
+        showError(input, atLeastCharMsg(input, min));
+    } else if (fieldLength > max) {
+        showError(input, lessThanCharMsg(input, max));
+    } else {
+        showError(input);
+    }
 }
 
 function checkIfRequired(fieldsArr) {
@@ -39,12 +62,27 @@ function checkIfRequired(fieldsArr) {
     })
 }
 
-function isEmailValid(email) {
+function isEmailValid(input) {
     const regexEmail = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
-    return regexEmail.test(String(email).toLowerCase());
+    if (regexEmail.test(input.value.trim())) {
+        showSuccess(input);
+    } else {
+        checkLength(input, 6, 25);
+        showError(input, 'Email is not valid');
+    }
+}
+
+function checkPasswordsMatch(pswrd1, pswrd2) {
+    if (pswrd1.value !== pswrd2.value) {
+        showError(pswrd2, 'Passwords do not match');
+    }
 }
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     checkIfRequired([username, email, password, password2]);
+    checkLength(username, 3, 15);
+    checkLength(password, 6, 25);
+    isEmailValid(email);
+    checkPasswordsMatch(password, password2);
 });
